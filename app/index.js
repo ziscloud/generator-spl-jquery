ffdae68b8d53ae9dc3356161ac66076917d92b65
Joakim Jarsäter <Joakim.Jarsater@postkodlotteriet.se>
2015-04-08 11:45:09 +0200

    fixed tests

--- app/index.js
+++ app/index.js
@@ -1,103 +1,101 @@
-'use strict';
-var util = require('util');
-var path = require('path');
-var generators = require('yeoman-generator');
+(function () {
+    'use strict';
+    var util = require('util');
+    var path = require('path');
+    var generators = require('yeoman-generator');
+    var mkdir = require("mkdirp");
 
-module.exports = generators.Base.extend({
-  constructor: function() {
-    generators.Base.apply(this, arguments);
+    module.exports = generators.Base.extend({
+        constructor: function () {
+            generators.Base.apply(this, arguments);
 
-    this.option('install', {defaults: true});
-    this.option('git',     {defaults: true});
-  },
+            this.option('install', {defaults: true});
+            this.option('git', {defaults: true});
+        },
 
-  promptTask: function() {
-    var done = this.async();
-    this.prompt({
-      name:    'pluginName',
-      message: 'What is the name of the plugin?',
-      default: process.cwd().split(path.sep).reverse()[0]
-    }, function(answer) {
-      this.pluginName = answer.pluginName;
+        promptTask: function () {
+            var done = this.async();
+            this.prompt({
+                name: 'pluginName',
+                message: 'What is the name of the plugin?',
+                "default": process.cwd().split(path.sep).reverse()[0]
+            }, function (answer) {
+                this.pluginName = answer.pluginName;
 
-      this.prompt({
-        type:    'confirm',
-        name:    'useLess',
-        message: 'Will this plugin use LESS as a CSS precompiler?',
-        default: true
-      }, function(answer) {
-        this.useLess = answer.useLess;
-        done();
-      }.bind(this));
-    }.bind(this));
-  },
+                this.prompt({
+                    type: 'confirm',
+                    name: 'useLess',
+                    message: 'Will this plugin use LESS as a CSS precompiler?',
+                    "default": true
+                }, function (answer) {
+                    this.useLess = answer.useLess;
+                    done();
+                }.bind(this));
+            }.bind(this));
+        },
 
-  createFolders: function() {
-    this.mkdir('src');
-    this.mkdir('test');
-  },
+        createGenericFiles: function () {
+            this.copy('jshintrc', '.jshintrc');
+            this.copy('gitignore', '.gitignore');
+        },
 
-  createGenericFiles: function() {
-    this.copy('jshintrc', '.jshintrc');
-    this.copy('gitignore', '.gitignore');
-  },
+        createMainJsAndTestFiles: function () {
 
-  createMainJsAndTestFiles: function() {
+            this.fs.copyTpl(
+                this.templatePath('_main.js'),
+                this.destinationPath('src/' + this.pluginName + '.js'),
+                { pluginName: this.pluginName, _: this._ }
+            );
 
-    this.fs.copyTpl(
-      this.templatePath('_main.js'),
-      this.destinationPath('src/' + this.pluginName + '.js'),
-      { pluginName: this.pluginName }
-    );
+            this.fs.copyTpl(
+                this.templatePath('_mainSpec.js'),
+                this.destinationPath('test/' + this.pluginName + 'Spec.js'),
+                { pluginName: this.pluginName, _: this._ }
+            );
+        },
 
-    this.fs.copyTpl(
-      this.templatePath('_mainSpec.js'),
-      this.destinationPath('test/' + this.pluginName + 'Spec.js'),
-      { pluginName: this.pluginName }
-    );
-  },
+        createLessFilesIfNeeded: function () {
+            if (this.useLess) {
+                this.fs.copyTpl(
+                    this.templatePath('_main.less'),
+                    this.destinationPath('src/' + this.pluginName + '.less'),
+                    { pluginName: this.pluginName, _: this._ }
+                );
+            }
+        },
 
-  createLessFilesIfNeeded: function() {
-    if (this.useLess) {
-      this.fs.copyTpl(
-        this.templatePath('_main.less'),
-        this.destinationPath('src/' + this.pluginName + '.less'),
-        { pluginName: this.pluginName }
-      );
-    }
-  },
+        createDependenciesAndSetupFiles: function () {
+            this.fs.copyTpl(
+                this.templatePath('_package.json'),
+                this.destinationPath('package.json'),
+                { pluginName: this.pluginName, useLess: this.useLess, _: this._ }
+            );
+            this.fs.copyTpl(
+                this.templatePath('_bower.json'),
+                this.destinationPath('bower.json'),
+                { pluginName: this.pluginName, useLess: this.useLess, _: this._ }
+            );
+            this.fs.copyTpl(
+                this.templatePath('_karma.conf.js'),
+                this.destinationPath('karma.conf.js'),
+                { pluginName: this.pluginName, _: this._ }
+            );
+            this.fs.copyTpl(
+                this.templatePath('_Gruntfile.js'),
+                this.destinationPath('Gruntfile.js'),
+                { pluginName: this.pluginName, useLess: this.useLess, _: this._ }
+            );
+        },
 
-  createDependenciesAndSetupFiles: function() {
-    this.fs.copyTpl(
-      this.templatePath('_package.json'),
-      this.destinationPath('package.json'),
-      { pluginName: this.pluginName, useLess: this.useLess }
-    );
-    this.fs.copyTpl(
-      this.templatePath('_bower.json'),
-      this.destinationPath('bower.json'),
-      { pluginName: this.pluginName, useLess: this.useLess }
-    );
-    this.fs.copyTpl(
-      this.templatePath('_karma.conf.js'),
-      this.destinationPath('karma.conf.js'),
-      { pluginName: this.pluginName }
-    );
-    this.fs.copyTpl(
-      this.templatePath('_Gruntfile.js'),
-      this.destinationPath('Gruntfile.js'),
-      { pluginName: this.pluginName, useLess: this.useLess }
-    );
-  },
+        end: function () {
+            if (this.options.install) {
+                this.installDependencies();
+                this.spawnCommand('grunt', ['dist'])
+            }
 
-  end: function() {
-    if (this.options.install) {
-      this.installDependencies();
-      this.spawnCommand('grunt', ['dist'])
-    }
-
-    if (this.options.git) {
-      this.spawnCommand('git', ['init']);
-    }
-  }
-});
+            if (this.options.git) {
+                this.spawnCommand('git', ['init']);
+            }
+        }
+    });
+}());
--- test/test-files.js
+++ test/test-files.js
@@ -1,85 +1,84 @@
-/*global describe, beforeEach, it*/
+/*global describe, beforeEach, it, after, require, __dirname*/
 'use strict';
 
 var assert = require('assert');
-var path    = require('path');
+var path = require('path');
 var helpers = require('yeoman-generator').test;
 var fs = require('fs');
 
-var TEST_PATH = './.temp';
-var TEST_NAME = 'test-name';
-var TEST_CMD  = 'spl-jquery:app';
-
 describe('spl-jquery', function () {
 
-  describe('with default options', function() {
-    beforeEach(function (done) {
-      var deps = [
-        [helpers.createDummyGenerator(), TEST_CMD],
-      ];
+    var deps = [
+        [helpers.createDummyGenerator(), 'spl-jquery:app']
+    ];
 
-      helpers.run(path.join(__dirname, '../app'))
-          .inDir(path.join(__dirname, TEST_PATH))
-          .withGenerators(deps)
-          .withOptions({install: false, git: false})
-          .withPrompt({pluginName: TEST_NAME})
-          .on('end', done);
-    });
+    describe('with default options', function () {
 
-    it('creates required files', function () {
-      var expected = [
-          ['.jshintrc', /"SPL": false/],
-
-          ['.gitignore', /bower_components\//],
-          ['.gitignore', /node_modules\//],
-
-          ['package.json', /"name": "test-name"/],
-          ['bower.json', /"name": "test-name"/],
-
-          ['src/test-name.js', /\$\.fn\.testName/],
-          ['src/test-name.js', /TestName = function TestName()/]
-      ];
-      assert.fileContent(expected);
-    });
-  });
-
-
-  describe('with with less', function() {
-    before(function (done) {
-      var deps = [
-        [helpers.createDummyGenerator(), TEST_CMD],
-      ];
-
-      helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(__dirname, TEST_PATH))
-      .withGenerators(deps)
-      .withOptions({install: false, git: false})
-      .withPrompt({pluginName: TEST_NAME})
-      .on('end', done);
-    });
-
-    it('creates required files', function () {
-      var expected = [
-          ['src/test-name.less', /\.test-name {/]
-      ];
-
-      assert.fileContent(expected);
-    });
-  });
-
-  after(function() {
-    var deleteFolderRecursive = function(path) {
-      if( fs.existsSync(path) ) {
-        fs.readdirSync(path).forEach(function(file,index){
-          var curPath = path + "/" + file;
-          if(fs.lstatSync(curPath).isDirectory()) { // recurse
-            deleteFolderRecursive(curPath);
-          } else { // delete file
-            fs.unlinkSync(curPath);
-          }
+        beforeEach(function (done) {
+            helpers.run(path.join(__dirname, '../app'))
+                .inDir(path.join(__dirname, './.temp'))
+                .withGenerators(deps)
+                .withOptions({install: false, git: false})
+                .withPrompt({pluginName: 'test-name'})
+                .on('end', done);
         });
-        fs.rmdirSync(path);
-      }
-    }(TEST_PATH);
-  });
+
+        it('creates required files', function () {
+            var expected = [
+                ['.jshintrc', /"SPL": false/],
+
+                ['.gitignore', /bower_components\//],
+                ['.gitignore', /node_modules\//],
+
+                ['package.json', /"name": "test-name"/],
+                ['bower.json', /"name": "test-name"/],
+
+                ['src/test-name.js', /\$\.fn\.testName/],
+                ['src/test-name.js', /TestName = function TestName/]
+            ];
+            assert.fileContent(expected);
+        });
+
+    });
+
+
+    describe('with with less', function () {
+
+        before(function (done) {
+            var a = helpers.run(path.join(__dirname, '../app'));
+
+            a.inDir(path.join(__dirname, './.temp'));
+            a.withGenerators(deps);
+            a.withPrompt({pluginName: 'test-name'});
+            a.withOptions({install: false, git: false});
+            a.on('end', done);
+        });
+
+        it('creates required files', function () {
+            var expected = [
+                ['src/test-name.less', /\.test-name \{/]
+            ];
+
+            assert.fileContent(expected);
+        });
+
+    });
+
+    after(function () {
+        var deleteFolderRecursive = function (path) {
+            if (fs.existsSync(path)) {
+                fs.readdirSync(path).forEach(function (file, index) {
+                    var curPath = path + "/" + file;
+                    if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                        deleteFolderRecursive(curPath);
+                    } else { // delete file
+                        fs.unlinkSync(curPath);
+                    }
+                });
+                fs.rmdirSync(path);
+            }
+        }('./.temp');
+
+    });
+
 });
